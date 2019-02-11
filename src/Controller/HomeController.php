f84c15a27202701580f9b8f212ef102578a8140b
BasharAlayan <alayanbashar@gmail.com>
2019-02-11 12:30:04 +0100

    Use Routing in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,37 @@
 namespace App\Controller;
 
 
+#use http\Env\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class HomeController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage(){
+
         return new Response('OMG! My first sympfony page ! :D');
+
     }
+    /**
+     * @Route("/article/toto")
+     */
+ //   public function read(){
+
+  //      return new Response("L'article Toto ");
+
+   // }
+
+    /**
+     * @Route("/article/{page}")
+     */
+     public function read(){
+
+          return new Response("L'article est Trouvé ");
+
+    }
+
 }
